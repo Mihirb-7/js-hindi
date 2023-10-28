4a27a9ba6bfa1183495d0b2b9253f07d671b9c4d
Mihirb-7 <116669365+Mihirb-7@users.noreply.github.com>
2023-10-28 12:22:08 +0000

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -4,7 +4,8 @@ console.log(typeof score);
 console.log(typeof(score));    // method
 
 let valueNumber = Number(score)
-console.log(typeof valueNumber); //if score is 34,44 etc then it is converted but what about 34abc , for 34abc it again output number but the value is NaN (not a no)
+console.log(typeof valueNumber); 
+//if score is 34,44 etc then it is converted but what about 34abc , for 34abc it again output number but the value is NaN (not a no)
 //her efor checkiing we should also check value of valueNumber for verification
 
 //in case of null the value become 0
@@ -15,4 +16,22 @@ console.log(typeof valueNumber); //if score is 34,44 etc then it is converted bu
 "33" => 33
 "33abc" => NaN
 */
+
+
+
+
+//**************operation *******************
+
+let value =3
+let negValue = -value  //assigning -ve value 
+
+console.log("1" + 2)  //12
+console.log(1 + "2")   //12
+console.log("1" + 2+2);  //122
+console.log(1+2+"2");    //32
+
+console.log(+true);   //output 1    
+console.log(+"");   //output 0    
+//console.log(true+);   //error
+
 

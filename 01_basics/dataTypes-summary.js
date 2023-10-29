f6db9f6fb43f9d7b0dfbc99d1c44d424a1589fe3
Mihirb-7 <116669365+Mihirb-7@users.noreply.github.com>
2023-10-29 09:51:10 +0000

    dataTypes summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,45 @@
+//datatypes is categorized into 2 types based on how data is stored and accessed in memory 
+
+//  Primitive -- call by value
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+//js is dynamically typed lang means data types of variables are determined by the value they hold at runtime
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+  
+// console.log(id === anotherId);       output false
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference type(Non primitive)
+
+// Array, Objects, Functions
+//the typeof return object for all refernce dataTypes
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+//object
+
+let myObj = {
+    name: "hitesh",     //written in key vaalue pair
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);    // the typeof operator print dataType of null = object 
+//the typeof operator print dataType of fuction = function / function object 
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3

5513db7673a8e5f04a56f41b2457f3f13455df0b
Mihirb-7 <116669365+Mihirb-7@users.noreply.github.com>
2023-10-30 14:17:31 +0000

    controlFlow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,8 @@
+//if
+// == check value equal or not , === also check the datatype of two values
+
+//implicit scope -- without {}
+const balance = 1000
+if(balance > 500) console.log("test");
+
+//if else
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,11 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+//if break is not there in any case then it executes all the case after the condition met except default
+
